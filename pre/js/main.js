75b0fed43932e3d474d20749fe4592801adefb8e
CarlosMunozDiazCSIC <carlos.munoz@cchs.csic.es>
2021-09-06 11:54:31 +0200

    Continuación del desarrollo de los elementos

--- pre/js/main.js
+++ pre/js/main.js
@@ -1,3 +1,33 @@
 
 //Necesario para importar los estilos de forma automática en la etiqueta 'style' del html final
 import '../css/main.scss';
+
+
+
+
+//Cambios de pestañas
+let tabs = document.getElementsByClassName('tab');
+let contenidos = document.getElementsByClassName('content');
+
+for(let i = 0; i < tabs.length; i++) {
+    tabs[i].addEventListener('click', function(e) {
+        displayContainer(e.target);
+    });
+}
+
+function displayContainer(elem) {
+    let content = elem.getAttribute('data-target');
+
+    //Poner activo el botón
+    for(let i = 0; i < tabs.length; i++) {
+        tabs[i].classList.remove('active');
+    }
+    elem.classList.add('active');
+
+    //Activar el contenido
+    for(let i = 0; i < contenidos.length; i++) {
+        contenidos[i].classList.remove('active');
+    }
+
+    document.getElementsByClassName(content)[0].classList.add('active');
+}
