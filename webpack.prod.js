97fa9801cd7ed541bb27bd451c54b16784c49aca
CarlosMunozDiazCSIC <carlos.munoz@cchs.csic.es>
2021-09-06 13:59:02 +0200

    Cambio de nombre de carpeta para Github Page

--- webpack.prod.js
+++ webpack.prod.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
 	mode: "production",
 	output: {
 		filename: "main.[contentHash].js",
-		path: path.resolve(__dirname, "prod/")
+		path: path.resolve(__dirname, "docs/")
 	},
 	optimization: {
 		minimizer: [
